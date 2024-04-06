a5453e654350c554b8e76b9e398effb59f5c0897
steve albers <stevealbers@hotmail.com>
2024-04-06 00:26:44 -0400

    exercise 06 add onsubmit to a form

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -9,21 +9,27 @@ function UsernameForm({onSubmitUsername}) {
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    //const username = event.target.elements[0].value
+    const username = event.target.elements.usernameInput.value;
+    onSubmitUsername(username)
+  }
 
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={(e)=>handleSubmit(e)}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input type="text" id="usernameInput"/>
       </div>
       <button type="submit">Submit</button>
     </form>
