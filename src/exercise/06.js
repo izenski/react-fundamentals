4974ea31b831791575cd8c52d1a64ff3c0fc91f2
steve albers <stevealbers@hotmail.com>
2024-04-06 00:39:51 -0400

    Use a ref to access a form field

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,35 +1,27 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import * as React from 'react';
+import { useRef } from 'react';
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+
+  const usernameInputRef = useRef(null);
 
   const handleSubmit = event => {
     event.preventDefault()
     //const username = event.target.elements[0].value
-    const username = event.target.elements.usernameInput.value;
+    //const username = event.target.elements.usernameInput.value;
+    const username=usernameInputRef.current.value;
+
     onSubmitUsername(username)
   }
 
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
     <form onSubmit={(e)=>handleSubmit(e)}>
       <div>
         <label>Username:</label>
-        <input type="text" id="usernameInput"/>
+        <input type="text" id="usernameInput" ref={usernameInputRef}/>
       </div>
       <button type="submit">Submit</button>
     </form>
