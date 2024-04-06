453a8871c26d320f0f4d98f205e19bce66a7dbef
steve albers <stevealbers@hotmail.com>
2024-04-05 22:37:24 -0400

    exercise 5 create a custom component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,16 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const Box = (boxSize,bgColor) => <div className={`box box--${boxSize}`} style={{fontStyle: 'italic', backgroundColor: bgColor}}>{boxSize} {bgColor} box</div>;
+
+
+const smallBox = Box("small","lightblue");
+const mediumBox = Box("medium","pink");
+const largeBox = Box("large","orange");
+
+// const smallBox = <div className="box box--small" style={{fontStyle: 'italic',backgroundColor: 'lightblue'}} >small lightblue box</div>
+//const mediumBox = <div className="box box--medium" style={{fontStyle: 'italic', backgroundColor: 'pink'}}>medium pink box</div>
+//const largeBox = <div className="box box--large"  style={{fontStyle: 'italic', backgroundColor: 'orange'}}>large orange box</div>
 
 function App() {
   return (
