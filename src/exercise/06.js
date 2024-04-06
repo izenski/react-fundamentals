1521cc523719b381349c29899b515234a89d30d6
steve albers <stevealbers@hotmail.com>
2024-04-06 00:59:51 -0400

    06 extracredit 02 add an OnChange handler and validation

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -7,6 +7,8 @@ import { useRef } from 'react';
 function UsernameForm({onSubmitUsername}) {
 
   const usernameInputRef = useRef(null);
+  const submitButton = useRef(null);
+  const [invalidChar, setInvalidChar] = React.useState('');
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -17,13 +19,29 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(username)
   }
 
+  const handleChange = (event) => {
+    //const value = event.target.value[event.target.value.length-1];
+    const value = usernameInputRef.current.value[usernameInputRef.current.value.length-1];
+    const isValid = value === value.toLowerCase();
+    if (!isValid){
+      setInvalidChar(value);
+      submitButton.current.disabled = true;
+    }
+    else {
+      setInvalidChar('');
+      submitButton.current.disabled = false;
+    }
+    setInvalidChar(isValid ? '' : value[value.length-1]);
+  }
+
   return (
     <form onSubmit={(e)=>handleSubmit(e)}>
+      <div>{invalidChar ?`The character ${invalidChar} is not permitted`: ""}</div>
       <div>
         <label>Username:</label>
-        <input type="text" id="usernameInput" ref={usernameInputRef}/>
+        <input type="text" id="usernameInput" ref={usernameInputRef} onChange={(e)=>handleChange(e)} />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" ref={submitButton}>Submit</button>
     </form>
   )
 }
